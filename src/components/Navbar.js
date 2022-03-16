e605c99585447de1f1aad70227ba679fb942b0ba
Gabrielle Ford <gabrielledeva@gmail.com>
2022-03-16 10:39:57 -0400

    navbar scrolls to sections

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,21 +10,45 @@ const Navbar = () => {
         <div className='flex justify-evenly'>
           <Link
             className='text-xl text-dark-teal px-3 py-0.5 border-r-2 border-dark-peach h-fit cursor-pointer rounded-l-lg hover:bg-light-peach navLink'
+            isDynamic={true}
+            hashSpy={true}
+            to='about'
+            smooth={true}
+            duration={500}
+            spy={true}
           >
             About
           </Link>
           <Link
             className='text-xl text-dark-teal px-3 py-0.5 border-r-2 border-dark-peach h-fit cursor-pointer hover:bg-light-peach navLink'
+            isDynamic={true}
+            hashSpy={true}
+            to='projects'
+            smooth={true}
+            duration={500}
+            spy={true}
           >
             Projects
           </Link>
           <Link
             className='text-xl text-dark-teal px-3 py-0.5 border-r-2 border-dark-peach h-fit cursor-pointer hover:bg-light-peach navLink'
+            isDynamic={true}
+            hashSpy={true}
+            to='devtools'
+            smooth={true}
+            duration={500}
+            spy={true}
           >
             Dev Tools
           </Link>
           <Link
             className='text-xl text-dark-teal px-3 py-0.5 h-fit cursor-pointer rounded-r-lg hover:bg-light-peach navLink'
+            isDynamic={true}
+            hashSpy={true}
+            to='contact'
+            smooth={true}
+            duration={500}
+            spy={true}
           >
             Contact
           </Link>
