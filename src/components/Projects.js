f76cb339f1132e418dd11c10a7d0ab8e9ee165a8
Gabrielle Ford <gabrielledeva@gmail.com>
2023-01-20 13:47:23 -0500

    updated projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BlissEventSpace, MealPrepMarket, RickAndMortyDatabase, ThreeDayProject } from "./Reusable/Data";
+import { BlissEventSpace, MealPrepMarket, RickAndMortyDatabase } from "./Reusable/Data";
 import ProjectInfo from "./Reusable/ProjectInfo";
 
 
@@ -7,7 +7,7 @@ const Projects = ({ projects }) => {
     return (
       <div id={projects} className='pt-6 lg:py-12'>
           <h2 className='cursor-default text-3xl text-center mb-7 lg:mb-8 lg:text-justify lg:ml-48 sm:text-5xl text-dark-teal font-display sectionHeader'>Projects</h2>
-          <div className="mx-auto grid grid-cols-1 lg:grid-cols-2 gap-0 max-w-7xl max-h-7xl">
+          <div className="mx-auto grid grid-cols-1 gap-0 max-w-7xl max-h-7xl lg:max-w-4xl lg:max-h-4xl">
             <div className='relative sm:flex col-span-1 sm:overflow-hidden'>
               <ProjectInfo {...BlissEventSpace} />
             </div>
@@ -17,9 +17,9 @@ const Projects = ({ projects }) => {
             {/* <div className='relative sm:flex col-span-1 sm:overflow-hidden'>
               <ProjectInfo {...InstaPet} />
             </div> */}
-            <div className='relative sm:flex col-span-1 sm:overflow-hidden'>
+            {/* <div className='relative sm:flex col-span-1 sm:overflow-hidden'>
               <ProjectInfo {...ThreeDayProject} />
-            </div>
+            </div> */}
             <div className='relative sm:flex col-span-1 sm:overflow-hidden'>
               <ProjectInfo {...RickAndMortyDatabase} />
             </div>
