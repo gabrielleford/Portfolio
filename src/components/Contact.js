f730341e0f5269c2a6e43cda145b12523f0b1a37
Gabrielle Ford <gabrielledeva@gmail.com>
2023-06-14 22:22:01 -0400

    fixed messages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,7 +18,7 @@ const Contact = ({ contact }) => {
                   </div>
                   <div className="grid">
                     <label htmlFor='message' className="text-dark-teal font-display formLabel text-xl">Message</label>
-                    <textarea type='text' id='message' className="rounded-sm focus:outline-1 focus:outline-dark-teal px-1 shadow-lg" required />
+                    <textarea name='message' type='text' id='message' className="rounded-sm focus:outline-1 focus:outline-dark-teal px-1 shadow-lg" required />
                   </div>
                   <button type='submit' className="font-poppins-light text-dark-teal text-xl w-fit px-3 py-1 mx-auto bg-peach shadow-lg rounded-[5px] transition-all duration-300 ease-in-out border border-light-peach/75 hover:bg-light-peach formBtn">Submit</button>
                 </form>
